b15243f262e525dc034f399a29c24df39b2782fa
Visha952 <visha3664@gmail.com>
2025-11-06 10:22:41 +0530

    Create Student.java

---
+++ Student.java
@@ -0,0 +1,19 @@
+package model;
+
+public class Student {
+    private int id;
+    private String name;
+    private String course;
+    private double fee;
+
+    public Student() {}
+
+    public Student(int id, String name, String course, double fee) {
+        this.id = id;
+        this.name = name;
+        this.course = course;
+        this.fee = fee;
+    }
+
+    // Getters and Setters
+}
